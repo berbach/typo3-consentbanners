c4df80cdc3bc22a78dfc5ffbac4f7497b9521676
Schulze <schulze@berbach.de>
2023-08-23 12:40:50 +0200

    fixed TCA Ultility Helper

--- Classes/Utility/TCASelectItemUtility.php
+++ Classes/Utility/TCASelectItemUtility.php
@@ -10,12 +10,17 @@ class TCASelectItemUtility
     public function getAllContentElements(&$params): void
     {
         $groups = BackendUtility::getPagesTSconfig(0)['mod.']['wizards.']['newContentElement.']['wizardItems.'];
-        $removeItems = GeneralUtility::trimExplode(',',BackendUtility::getPagesTSconfig(0)['TCEFORM.']['tt_content.']['CType.']['removeItems']);
+        $CType = isset(BackendUtility::getPagesTSconfig(0)['TCEFORM.']['tt_content.']['CType.']) ? BackendUtility::getPagesTSconfig(0)['TCEFORM.']['tt_content.']['CType.'] : null;
+        $removeItems = !is_null($CType) && isset($CType['removeItems']) ? $CType['removeItems'] : null;
+        $removeItems = !is_null($removeItems) ? GeneralUtility::trimExplode(',',$removeItems) : null;
         foreach ($groups as $key => $group) {
             $params['items'][] = ['label' => $group['header'], 'value' => '--div--', 'group' => rtrim($key, '.')];
             foreach ($group['elements.'] as $element) {
 
-                if(!in_array($element['tt_content_defValues.']['CType'], $removeItems, true)) {
+                if(!is_null($removeItems) && !in_array($element['tt_content_defValues.']['CType'], $removeItems, false)) {
+                    $params['items'][] = ['label' => $element['title'], 'value' => $element['tt_content_defValues.']['CType'], 'icon' => $element['iconIdentifier'], 'group' => rtrim($key, '.')];
+                }
+                if (is_null($removeItems)){
                     $params['items'][] = ['label' => $element['title'], 'value' => $element['tt_content_defValues.']['CType'], 'icon' => $element['iconIdentifier'], 'group' => rtrim($key, '.')];
                 }
             }
